c9ec4ba1917558239c1846b531e013f273bea3c3
abhinay chaturvedi <abhinaychaturvedi88@gmail.com>
2023-10-21 10:49:54 +0530

    added zoom in out animaiton

--- src/components/HomePage/HeroSection/HeroSectionItem.jsx
+++ src/components/HomePage/HeroSection/HeroSectionItem.jsx
@@ -1,11 +1,14 @@
 import { Box } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
-
+const animation1 = {
+  "0%": {transform: "scale(1)"},
+  "100%": {transform: "scale(1.1)"}
+}
 const HeroSectionItem = ({color, imgUrl}) => {
   return (
-    <Box sx={{flex: "0 0 100%", position: "relative", background: "blur", height: "100vh","&::before":{ background: "rgba(0,0,0,.65)", position: "absolute", width: "100%", height: "100%", content: '""', borderRadius: "10px"}}}>
-      <Image sizes="100vw" width={0} height={0} style={{width: "100%", height: "100%",}}  src={imgUrl}/>
+    <Box sx={{ "@keyframes updown": animation1,overflow: "hidden", width: "100vw",flex: "1 0 100%", position: "relative", background: "blur", height: "100vh","&::before":{ background: "rgba(0,0,0,.65)", position: "absolute", width: "100%", height: "100%", content: '""', borderRadius: "10px", zIndex: 1}}}>
+      <Image sizes="100vw" width={0} height={0} style={{animation: "updown 3s ease-in-out infinite alternate",width: "100%", height: "100%",  }}  src={imgUrl}/>
     </Box>
   )
 }
