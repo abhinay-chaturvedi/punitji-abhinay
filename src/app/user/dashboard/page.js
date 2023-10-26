bfc0385850a6a4a3cfc948afa73955acac4e5aef
abhinay chaturvedi <abhinaychaturvedi88@gmail.com>
2023-10-26 23:40:55 +0530

    position fixed of left bar

--- src/app/user/dashboard/page.js
+++ src/app/user/dashboard/page.js
@@ -1,22 +1,35 @@
 'use client'
 import React from 'react'
-import {Box, Grid, Avatar} from "@mui/material"
+import {Box, Grid, Avatar, useTheme, useMediaQuery, Button} from "@mui/material"
 import LeftBar from '@/components/UserDashboard/LeftBar';
 import { useRouter } from 'next/navigation';
 const Page = () => {
 
   const router = useRouter();
   console.log("router object", router);
+  const theme = useTheme();
+  const mdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const sideBarStyle = {
+    [theme.breakpoints.down('md')]: {
+      position: 'fixed', 
+      backgroundColor: "pink", 
+      width: 280, 
+      top: 0,
+      transform: "translateX(-280px)",
+      // transform: "translateX(0px)"
+    }
+  }
   return (
     <Box>
         <Grid container>
-            <Grid item md={2.5}>
-              <Box sx={{bgcolor: "whitesmoke", minHeight: "100vh", borderRight: "1px solid black"}}>
+            <Grid item sx={sideBarStyle} md={2.5}>
+              <Box sx={{minHeight: "100vh", borderRight: "1px solid black"}}>
                 <LeftBar/>
               </Box>
+              <Button sx={{position: "absolute",right: "-20%", top: "50%"}}>open</Button>
             </Grid>
-            <Grid item md={9.5}>
-              <Box sx={{bgcolor: "whitesmoke", minHeight: "100vh"}}>ksjdlks</Box>
+            <Grid item xs= {12} md={9.5}>
+              <Box sx={{minHeight: "100vh"}}>ksjdlks</Box>
             </Grid>
         </Grid>
     </Box>
@@ -24,3 +37,6 @@ const Page = () => {
 }
 
 export default Page
+const a = {
+
+}
--- src/components/UserDashboard/LeftBar.jsx
+++ src/components/UserDashboard/LeftBar.jsx
@@ -22,27 +22,27 @@ const LeftBar = () => {
           <Divider />
           <Box>
               <Stack>
-                  <Box component={Link} href="/user/dashboard" sx={{display: "flex", bgcolor: !q?"white":"", gap: 2,alignItems: "center", p: "10px"}}>
+                  <Box component={Link} href="/user/dashboard" sx={{display: "flex", bgcolor: !q?"whitesmoke":"", gap: 2,alignItems: "center", p: "10px"}}>
                       <DashboardIcon/>
                       <Typography component="span" sx={{color: "gray", fontWeight: "bold"}}>Dashboard</Typography>
                   </Box>
                   <Divider sx={{width: "100%"}} />
-                  <Box component={Link} href="/user/dashboard/?page=services" sx={{bgcolor: q==="services"?"white":"",display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
+                  <Box component={Link} href="/user/dashboard/?page=services" sx={{bgcolor: q==="services"?"whitesmoke":"",display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
                       <DashboardIcon/>
                       <Typography component="span" sx={{color: "gray", fontWeight: "bold"}}>Services</Typography>
                   </Box>
                   <Divider sx={{width: "100%"}} />
-                  <Box component={Link} href="/user/dashboard/?page=info" sx={{bgcolor: q==="info"?"white":"", display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
+                  <Box component={Link} href="/user/dashboard/?page=info" sx={{bgcolor: q==="info"?"whitesmoke":"", display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
                       <DashboardIcon/>
                       <Typography component="span" sx={{color: "gray", fontWeight: "bold"}}>Info</Typography>
                   </Box>
                   <Divider sx={{width: "100%"}} />
-                  <Box component={Link} href="/user/dashboard/?page=process" sx={{bgcolor: q==="process"?"white":"", display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
+                  <Box component={Link} href="/user/dashboard/?page=process" sx={{bgcolor: q==="process"?"whitesmoke":"", display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
                       <DashboardIcon/>
                       <Typography component="span" sx={{color: "gray", fontWeight: "bold"}}>Track Process</Typography>
                   </Box>
                   <Divider sx={{width: "100%"}} />
-                  <Box component={Link} href="/user/dashboard/?page=documents" sx={{bgcolor: q==="documents"?"white":"", display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
+                  <Box component={Link} href="/user/dashboard/?page=documents" sx={{bgcolor: q==="documents"?"whitesmoke":"", display: "flex", gap: 2,alignItems: "center", p: "10px"}}>
                       <DashboardIcon/>
                       <Typography component="span" sx={{color: "gray", fontWeight: "bold"}}>Documents</Typography>
                   </Box>
